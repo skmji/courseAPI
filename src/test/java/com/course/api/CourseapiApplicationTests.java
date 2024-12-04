baf01ee731f68eb77a25f8af061c0e81f0f86831
sanjay kumar maurya <skm.onlinemail@gmail.com>
2024-12-04 20:28:01 +0530

    first commit

---
+++ src/test/java/com/course/api/CourseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
